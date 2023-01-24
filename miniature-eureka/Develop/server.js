dbad4bc9f7cb4cc6059cb2ac68fe7cb6e78aace8
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-24 15:33:39 -0500

    added GET route to read json file

--- miniature-eureka/Develop/server.js
+++ miniature-eureka/Develop/server.js
@@ -1,8 +1,7 @@
 //Import dependencies
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 //Initialize the app
 const app = express();
@@ -15,17 +14,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Connect to static file directory
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //Connect to index (primary) page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 //Connect to notes page
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-
-
+//Read JSON file and get saved notes
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+        if(err) {
+            return console.error(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    })
+})
