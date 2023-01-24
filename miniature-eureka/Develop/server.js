affded333acb8a101ee8d05533ce0c19205838a2
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-24 15:24:54 -0500

    added basic dependencies and initialized app in server, added route dir and necessary routes

---
+++ miniature-eureka/Develop/server.js
@@ -0,0 +1,14 @@
+//Import dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+
+//Initialize the app
+const app = express();
+
+//Declare which port the app should listen to
+let PORT = process.env.PORT || 4000;
+
+
+
