72b381d3bacf39a8b60ff8a9c49dfddae3f8be3a
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-24 16:59:56 -0500

    started delete route

--- miniature-eureka/Develop/server.js
+++ miniature-eureka/Develop/server.js
@@ -39,43 +39,91 @@ app.get("/api/notes", (req, res) => {
 
 //POST route to take and save new notes
 app.post("/api/notes", (req, res) => {
+
+//Get data from the request body
   const dockedNote = req.body;
 
+//Read json file to get current notes
   fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
     if (err) {
       return console.error(err);
-    } 
+    }
+
+//Assign values in json file to array in temporary variable
     let notes = JSON.parse(data);
     console.log(notes);
-    if(notes.length > 0){
 
-    let lastID = notes[notes.length-1].id;
-    var id = parseInt(lastID + 1);
+    //If array has values, assign ID value to be one more than the previous ID
+    if (notes.length > 0) {
+      let lastID = notes[notes.length - 1].id;
+      var id = parseInt(lastID + 1);
+
+        //If array is vacant give the first note a value of one
     } else {
-        var id = 1;
+      var id = 1;
     }
 
+    //Create new note from data stored in the request body
     let savedNote = {
-        title: dockedNote.title,
-        text: dockedNote.text,
-        id: id
-    }
+      title: dockedNote.title,
+      text: dockedNote.text,
+      id: id,
+    };
 
+    //Copy the array of notes
     var notesArray = notes.slice(0);
+    //Push new note onto clone array and save to file
     notesArray.push(savedNote);
 
-    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notesArray), (err) => {
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(notesArray),
+      (err) => {
         if (err) {
           return console.error(err);
         } else {
           res.json(notesArray);
         }
-      });
+      }
+    );
   });
 });
 
 //DELETE route to delete a note
-//app.delete();
+app.delete("/api/notes/:id", (req, res) => {
+
+//Get ID from request parameters
+  let idToDelete = req.params.id;
+
+//Read the json file and assign array of values to a temporary variable
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    let notes = JSON.parse(data);
+    console.log(notes);
+
+    //Loop through the array until one has an ID that matches the one to be deleted
+    for (let i = 0; i < notes.length; i++) {
+      if (notes[i].id == idToDelete) {
+        var notesArray = notes.splice(i, 1);
+
+    //Save the altered array to the json file
+        fs.writeFile(
+          path.join(__dirname, "./db/db.json"),
+          JSON.stringify(notesArray),
+          (err, data) => {
+            if (err) {
+              return console.error(err);
+            }
+              res.json(notesArray);
+            })
+          }
+      }
+    });
+});
+
 
 //App listener on PORT
 app.listen(PORT, () => {
