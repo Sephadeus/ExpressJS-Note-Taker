1c79b796bdb55283c396153f33d44a39b9a0a160
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-01-24 15:26:16 -0500

    connected static files and added GET routes

--- miniature-eureka/Develop/server.js
+++ miniature-eureka/Develop/server.js
@@ -10,5 +10,22 @@ const app = express();
 //Declare which port the app should listen to
 let PORT = process.env.PORT || 4000;
 
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Connect to static file directory
+app.use(express.static(__dirname + '/public'));
+
+//Connect to index (primary) page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//Connect to notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
 
 
